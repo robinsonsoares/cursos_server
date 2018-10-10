f7a49d69633be681679677f8639ab15112759ee6
Robinson Soares <soares.robinson@gmail.com>
2018-10-10 09:49:35 -0300

    Integrando Angular com Express

--- config/express.js
+++ config/express.js
@@ -2,10 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var load = require('express-load');
+var cors = require('cors');
 
 module.exports = function () {
     var app = express();
 
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
